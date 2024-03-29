ad713ff859db9c32fdc339f1d087fdbba4e967b5
vuong20194412 <vuong.thcodeforces194412@outlook.com.vn>
2024-03-29 11:56:49 +0700

    Update middleware.py

--- mysite/polls/middleware.py
+++ mysite/polls/middleware.py
@@ -9,11 +9,6 @@ class TimezoneMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if 'can_request_from_datetime' in request.session:
-            #request.session.pop('can_request_from_datetime')
-            can_request_from_datetime = request.session.get('can_request_from_datetime')
-            if can_request_from_datetime and datetime.datetime.fromisoformat(can_request_from_datetime) > datetime.datetime.now(datetime.timezone.utc):
-                return HttpResponse('<h1>Error cookie or javascript disable</h1>', status=401)
         tz_sub_utc_minutes = request.COOKIES.get("tz_sub_utc_minutes")
         if tz_sub_utc_minutes:
             try:
@@ -21,30 +16,40 @@ class TimezoneMiddleware:
                 timezone.activate(datetime.timezone(datetime.timedelta(minutes=tz_sub_utc_minutes)))
             except ValueError:
                 timezone.activate(datetime.timezone.utc)
-                request.session['can_request_from_datetime'] = (datetime.datetime.now() + datetime.timedelta(minutes=1)).isoformat()
-                return HttpResponse('\n<script>'
-                                    '\n\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`;'
-                                    '\n\tlocation.reload();'
-                                    '\n</script>')
+                return HttpResponse(self.get_html_code_of_tz_sub_utc_minutes())
         else:
             timezone.activate(datetime.timezone.utc)
-            request.session['can_request_from_datetime'] = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=1)).isoformat()
-            return HttpResponse('\n<script>'
-                                '\n\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`;'
-                                '\n\tlocation.reload();'
-                                '\n</script>')
+            return HttpResponse(self.get_html_code_of_tz_sub_utc_minutes())
         response = self.get_response(request)
-        response.content = ("\n<script>"
-                             "\n\tfunction getCookie(name) {"
-                             "\n\t\tconst parts=`; ${document.cookie}`.split(`; ${name}=`);"
-                             "\n\t\tif (parts.length >= 2) {"
-                             "\n\t\t\tconst value = parts.pop().split(';').shift();"
-                             "\n\t\t\treturn value;"
-                             "\n\t\t}"
-                             "\n\t}"
-                             "\n\tif(getCookie('tz_sub_utc_minutes') != -(new Date()).getTimezoneOffset()) {"
-                             "\n\t\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`; "
-                             "\n\t\tlocation.reload();"
-                             "\n\t}"
-                             "\n</script>").encode() + response.content
+        response.content = self.get_html_code_of_tz_sub_utc_minutes().encode() + response.content
         return response
+
+    def get_html_code_of_tz_sub_utc_minutes(self):
+        text = ('\n<noscript><h1>Please enable javascript and try again.</h1></noscript>\n<script>'
+                '\n\tfunction getCookie(name) {'
+                '\n\t\tconst parts=`; ${document.cookie}`.split(`; ${name}=`);'
+                '\n\t\tif (parts.length >= 2) {'
+                '\n\t\t\tconst value = parts.pop().split(";").shift();'
+                '\n\t\t\treturn value;'
+                '\n\t\t}'
+                '\n\t}'
+                '\n\tif(!getCookie("tz_sub_utc_minutes")) {'
+                '\n\t\tconst cookies = document.cookie'
+                '\n\t\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`;'
+                '\n\t\tif(!getCookie("tz_sub_utc_minutes")) {'
+                '\n\t\t\tdebugger'
+                '\n\t\t\tdocument.write("<h1>Please enable cookies and try again.</h1>")'
+                '\n\t\t}'
+                '\n\t\telse {'
+                '\n\t\t\tdebugger'
+                '\n\t\t\tlocation.reload();'
+                '\n\t\t}'
+                '\n\t}'
+                '\n\telse if(getCookie("tz_sub_utc_minutes") != -(new Date()).getTimezoneOffset()) {'
+                '\n\t\tconst cookies = document.cookie'
+                '\n\t\tdebugger'
+                '\n\t\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`; '
+                '\n\t\tlocation.reload();'
+                '\n\t}'
+                '\n</script>')
+        return text
