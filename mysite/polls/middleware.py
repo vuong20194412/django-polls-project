b5a80a2cecaf6a43dad61b8876bf2972eec389d2
vuong20194412 <vuong.thcodeforces194412@outlook.com.vn>
2024-03-29 12:23:24 +0700

    Update middleware.py

--- mysite/polls/middleware.py
+++ mysite/polls/middleware.py
@@ -3,13 +3,15 @@ from django.utils import timezone
 
 import datetime
 
+TZ_SUB_UTC_MINUTES = 'tsum'#'tz_sub_utc_minutes'
+
 
 class TimezoneMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        tz_sub_utc_minutes = request.COOKIES.get("tz_sub_utc_minutes")
+        tz_sub_utc_minutes = request.COOKIES.get(TZ_SUB_UTC_MINUTES)
         if tz_sub_utc_minutes:
             try:
                 tz_sub_utc_minutes = int(tz_sub_utc_minutes)
@@ -25,7 +27,8 @@ class TimezoneMiddleware:
         return response
 
     def get_html_code_of_tz_sub_utc_minutes(self):
-        text = ('\n<noscript><h1>Please enable javascript and try again.</h1></noscript>\n<script>'
+        text = ('\n<noscript><h1>Please enable javascript and try again.</h1></noscript>'
+                '\n<script>'
                 '\n\tfunction getCookie(name) {'
                 '\n\t\tconst parts=`; ${document.cookie}`.split(`; ${name}=`);'
                 '\n\t\tif (parts.length >= 2) {'
@@ -33,10 +36,10 @@ class TimezoneMiddleware:
                 '\n\t\t\treturn value;'
                 '\n\t\t}'
                 '\n\t}'
-                '\n\tif(!getCookie("tz_sub_utc_minutes")) {'
+                '\n\tif(!getCookie("' + TZ_SUB_UTC_MINUTES + '")) {'
                 '\n\t\tconst cookies = document.cookie'
-                '\n\t\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`;'
-                '\n\t\tif(!getCookie("tz_sub_utc_minutes")) {'
+                '\n\t\tdocument.cookie=`' + TZ_SUB_UTC_MINUTES + '=${-(new Date()).getTimezoneOffset()};secure`;'
+                '\n\t\tif(!getCookie("' + TZ_SUB_UTC_MINUTES + '")) {'
                 '\n\t\t\tdebugger'
                 '\n\t\t\tdocument.write("<h1>Please enable cookies and try again.</h1>")'
                 '\n\t\t}'
@@ -45,11 +48,11 @@ class TimezoneMiddleware:
                 '\n\t\t\tlocation.reload();'
                 '\n\t\t}'
                 '\n\t}'
-                '\n\telse if(getCookie("tz_sub_utc_minutes") != -(new Date()).getTimezoneOffset()) {'
+                '\n\telse if(getCookie("' + TZ_SUB_UTC_MINUTES + '") != -(new Date()).getTimezoneOffset()) {'
                 '\n\t\tconst cookies = document.cookie'
                 '\n\t\tdebugger'
-                '\n\t\tdocument.cookie=`tz_sub_utc_minutes=${-(new Date()).getTimezoneOffset()}`; '
+                '\n\t\tdocument.cookie=`' + TZ_SUB_UTC_MINUTES + '=${-(new Date()).getTimezoneOffset()};secure`; '
                 '\n\t\tlocation.reload();'
                 '\n\t}'
-                '\n</script>')
+                '\n</script>\n')
         return text
